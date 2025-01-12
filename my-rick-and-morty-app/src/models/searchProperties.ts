f9497963de054c54e3c3f4e23bb9d460bfce9512
Laryssa Azevedo <lazevedo@viceri.com.br>
2025-01-12 00:23:05 -0300

    feat: create models

---
+++ my-rick-and-morty-app/src/models/searchProperties.ts
@@ -0,0 +1,18 @@
+export type PageTextMap ={
+    boxTitle: string;
+    title: string;
+    description: string;
+    hasButtonBack?: boolean;
+    hasSearch: boolean;
+}
+
+export type ResultsProps = {
+    title: string;
+    description: string;
+    hasButtonBack?: boolean
+}
+
+export type NavbarHomeProps = {
+    boxTitle: string;
+    hasSearch: boolean;
+}
