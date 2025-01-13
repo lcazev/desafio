ce5dd5f1cf253e88a27cc7d8b7bfa9100dead6f0
Laryssa Azevedo <lazevedo@viceri.com.br>
2025-01-12 23:39:46 -0300

    feat: implement logic to show favorites count

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,18 @@
 "use client";
 import { usePathname } from "next/navigation";
-import { Button, Group } from "@chakra-ui/react";
+import { Badge, Button, Group } from "@chakra-ui/react";
 import { MdHome } from "react-icons/md";
 import { FaHeart } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
+import { useCharacterStore } from "@/hooks/useCharacterStore";
 
 const Navbar
  = () => {
   const pathname = usePathname();
   const isHome = pathname === '/home';
+  const favoriteCharacterIds = useCharacterStore(state => state.favoriteCharacterIds)
+  const favoritesCount = favoriteCharacterIds.length;
   return (
     <>
     <div className="w-full bg-background flex items-center justify-between">
@@ -20,7 +23,11 @@ const Navbar
       <div className="flex gap-4">
       <Group attached>
       <Link href="/"><Button background={isHome ? "white": "black"} color={isHome ? "black": "white"} padding="1rem" ><MdHome/>Início</Button></Link>
-      <Link href="/favourites"><Button background={isHome ? "black": "white"} color={isHome ? "white": "black"} padding="1rem"><FaHeart/>Favoritos</Button></Link>
+      <Link href="/favorites"><Button background={isHome ? "black": "white"} color={isHome ? "white": "black"} padding="1rem"><FaHeart/>Favoritos  {favoritesCount > 0 && (
+                  <Badge ml={2} borderRadius="full" fontSize="sm" padding="0.2rem 0.4rem" bg={isHome ? "white": "black"} color={isHome ? "black": "white"}>
+                    {favoritesCount}
+                  </Badge>
+                )}</Button></Link>
       </Group>
       </div>
     </div>
