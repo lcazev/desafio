e8e923440daab4e54e945983efe5770916e75e1a
Laryssa Azevedo <lazevedo@viceri.com.br>
2025-01-12 23:39:16 -0300

    feat: implement logic to add favorite

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,26 +1,36 @@
+import { useCharacterStore } from "@/hooks/useCharacterStore";
 import { Card, Image, IconButton, Box } from "@chakra-ui/react";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai"; 
 
 type ProfileCardProps = {
+  id: number;
   name: string;
   gender: string;
   image: string;
-  isFavourite?: boolean;
-  onToggleFavourite?: () => void; 
 };
 
-const ProfileCard = ({ name, gender, image, isFavourite = false, onToggleFavourite }: ProfileCardProps) => {
+const ProfileCard = ({ name, gender, image, id}: ProfileCardProps) => {
+  const { favoriteCharacterIds, addFavoriteCharacter, removeFavoriteCharacter } = useCharacterStore();
+  const isFavorite = favoriteCharacterIds.includes(id);
+  const handleFavoriteClick = () => {
+    if (isFavorite) {
+      removeFavoriteCharacter(id);
+    } else {
+      addFavoriteCharacter(id);
+    }
+  };
+
   return (
     <Card.Root maxW="sm" border="1px solid gray" borderRadius="8px" position="relative">
       <Image src={image} alt="Avatar do personagem" />
       <Box position="absolute" top="4px" right="4px">
         <IconButton
-          aria-label="Add to favourites"
-          onClick={onToggleFavourite}
+          aria-label="Add to favorites"
+          onClick={handleFavoriteClick}
           background='white'
           color='black'
           size="lg"
-        >{isFavourite ? <AiFillHeart color="red" /> : <AiOutlineHeart />}</IconButton>
+        >{isFavorite ? <AiFillHeart color="black" /> : <AiOutlineHeart />}</IconButton>
       </Box>
       <Card.Body gap="2">
         <Card.Title fontFamily="creepster" fontSize="1.5rem">{name}</Card.Title>
